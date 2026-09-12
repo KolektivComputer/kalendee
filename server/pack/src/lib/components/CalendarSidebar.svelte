30392628a6e22cf5798874b287a957b0cdb8970e
Elizabeth Hazel Ainslie <mey@lizainslie.dev>
2026-09-12 02:25:41 -0500

    fix(web): place sidebar tooltips beside the clipped drawer edge

--- server/pack/src/lib/components/CalendarSidebar.svelte
+++ server/pack/src/lib/components/CalendarSidebar.svelte
@@ -492,7 +492,7 @@
     {#if !readOnly}
       <button
         type="button"
-        class="btn btn-ghost btn-xs btn-square tooltip"
+        class="btn btn-ghost btn-xs btn-square tooltip tooltip-right"
         data-tip="Add Calendar"
         aria-label="Add Calendar"
         onclick={openCreate}
@@ -555,7 +555,7 @@
       <h2 class="text-xs font-semibold tracking-wide text-base-content/50 uppercase">Friends</h2>
       <button
         type="button"
-        class="btn btn-ghost btn-xs btn-square tooltip"
+        class="btn btn-ghost btn-xs btn-square tooltip tooltip-right"
         data-tip="Add Friend"
         aria-label="Add Friend"
         onclick={openFriends}
